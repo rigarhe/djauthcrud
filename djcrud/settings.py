5cfc713116815b3c4c2e0cfb5e3c1e57bba6bfb9
rigarhe <ripgarciaherrera@gmail.com>
2023-02-15 17:40:35 -0500

    ultimo

--- djcrud/settings.py
+++ djcrud/settings.py
@@ -89,7 +89,14 @@ DATABASES = {
         conn_max_age=600
     )
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
